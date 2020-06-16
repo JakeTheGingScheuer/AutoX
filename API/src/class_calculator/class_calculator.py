d2b7822288c6831497f5bef28a97493c7e74d4d3
JakeTheGingScheuer <jacob.scheuer@accenture.com>
2020-06-15 23:35:14 -0400

    data can now be saved after changes and restored from a backup

--- API/src/class_calculator/class_calculator.py
+++ API/src/class_calculator/class_calculator.py
@@ -1,9 +1,11 @@
 import json
 
+BACKUP_DATA_LOCATION = '/Users/jacob.scheuer/Development/python/AutoXCalc/API/src/cars.json'
 
 class ClassCalculator:
     def __init__(self, data_location):
-        with open(data_location) as file:
+        self.data_location = data_location
+        with open(self.data_location) as file:
           self.data = json.load(file)
         self.manufactuers = self.data["manufactuers"]
 
@@ -15,3 +17,15 @@ class ClassCalculator:
 
     def remove_manufacturer(self, name_of_manufactuer):
         self.manufactuers.remove(name_of_manufactuer)
+
+    def save_changes(self):
+        with open(self.data_location, 'w') as file:
+            self.data = {"manufactuers" : self.manufactuers}
+            changes = json.dumps(self.data)
+            file.write(changes)
+
+    def restore_data(self):
+        with open(BACKUP_DATA_LOCATION) as file:
+            self.data = json.load(file)
+        self.manufactuers = self.data["manufactuers"]
+        self.save_changes()
--- API/src/class_calculator/test_class_calculator.py
+++ API/src/class_calculator/test_class_calculator.py
@@ -5,7 +5,7 @@ from class_calculator import ClassCalculator
 
 
 class TestClassCalculator(object):
-    data_location = os.getcwd()+'/src/cars.json'
+    data_location = os.getcwd()+'/src/test_data.json'
 
     def test_get_manufacturers_returns_list_of_manufacturers_from_json(self):
         uut = ClassCalculator(self.data_location)
@@ -24,3 +24,19 @@ class TestClassCalculator(object):
         uut.remove_manufacturer("Bugatti")
         list_of_manufacturers = uut.get_manufacturers()
         assert "Bugatti" not in list_of_manufacturers
+
+    def test_save_changes_updates_storage(self):
+        uut = ClassCalculator(self.data_location)
+        uut.add_manufacturer("Bugatti")
+        uut.save_changes()
+
+        uut2 = ClassCalculator(self.data_location)
+        list_of_manufacturers = uut2.get_manufacturers()
+        assert "Bugatti" in list_of_manufacturers
+
+    def test_restore_data_rolls_back_data(self):
+        uut = ClassCalculator(self.data_location)
+        uut.remove_manufacturer("Acura")
+        uut.restore_data()
+        list_of_manufacturers = uut.get_manufacturers()
+        assert "Acura" in list_of_manufacturers
