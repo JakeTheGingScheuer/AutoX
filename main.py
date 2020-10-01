cd1331ae6c8c67cd656b7933cf3d664b7fbd77fe
JakeTheGingScheuer <jacob.scheuer@accenture.com>
2020-10-01 17:37:34 -0400

    config

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!flask/bin/python
-import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 from src.api import Api
@@ -12,11 +11,11 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-@app.route('/api/street_class/')
+@app.route('/')
 def get_street_class_data():
     cars = api.get_car_data()
     return jsonify(cars), 200
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000), debug=True)
+    app.run()
